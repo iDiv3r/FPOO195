5a0bd090a166c8b404e30fa1719a55aca11d1901
Div3r <udhgjkf@gmail.com>
2024-01-24 20:23:44 -0600

    Finalizamos la práctica 2

--- sintaxis.py
+++ sintaxis.py
@@ -11,3 +11,81 @@ varias
 lineas
 '''
 
+# 2- Cadenas --------------------------------------------------------
+
+# print('soy una cadena')
+# print("soy otra cadena")
+
+# a = 'mi banda favorita es'
+# b = "grupo marrano"
+
+# print(a + b)
+# print(a,b)
+
+# # contar caracteres 
+# print(len(a))
+
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+
+# c = 'mi banda favorita es \n grupo marrano'
+# print(c)
+
+
+# 3- Variables ------------------------------------------------------
+
+# x = 5
+# y = "john"
+
+# print(x)
+# print(type(x))
+
+# x = "hola"
+
+# print(x)
+# print(y)
+
+# print(type(x))
+# print(type(y))
+
+# x2 = int(3)
+# y2 = str("hola")
+# z2 = float(3.2)
+
+# import random
+# numero = random.randrange(1,15)
+# print(numero)
+
+# 4- Solicitud de datos ----------------------------------------------
+
+# var1 = input("\n introduce cualquier dato") 
+# print(var1)
+
+# var2 = str(input("\n introduce una cadena de texto"))
+# print(var1)
+
+# var3 = int(input("\n introduce un numero entero"))
+# print(var1)
+
+# var4 = float(input("\n introduce un numero decimal")) 
+# print(var1)
+
+# print("\n" , var1 , var2 , var3 , var4)
+
+# 5- Operadores -------------------------------------------------------
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(10 >= 9)
+print(10 != 9)
+print(10 <= 9)
+
+print("-------")
+x = 1
+print(x < 5 and x < 10)
+print(x < 5 or x < 10)
+print(not(x < 5 and x < 10))
+
+
