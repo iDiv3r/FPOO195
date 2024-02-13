ea3b3174563e2db6ae4b827363f201ba1e8d0cfe
Div3r <udhgjkf@gmail.com>
2024-02-12 23:59:25 -0600

    Ejercicio 2 practica 6.1 terminada

--- Práctica7 (Listas y tuplas)/Ejercicio2.py
+++ Práctica7 (Listas y tuplas)/Ejercicio2.py
@@ -1,3 +1,49 @@
+def contRepeticiones(lista):
+    
+    repeticiones = []
+    i = 0
+    
+    while i < len(lista):
+        
+        contador = 0
+        j = 0 
+        while j < len(lista):
+            
+            if lista[j] == lista [i]:
+                contador += 1
+            
+            j += 1
+        
+        repeticiones.append(contador)
+        i += 1
+    
+    return repeticiones
+
+
+def eliminarRepetidos(lista):
+    
+    lista2 = []
+
+    for numero in lista:
+        if numero not in lista2:
+            lista2.append(numero)
+            
+    
+    return lista2
+
+def reemplazarACero(lista):
+    lista2 = []
+
+    for numero in lista:
+        if numero not in lista2:
+            lista2.append(numero)
+        else:
+            lista2.append(0)
+            
+    
+    return lista2
+#---------------------------------------------------------
+
 import random
 
 lista = []
@@ -8,4 +54,64 @@ while i < 30:
     i += 1
     
 
-print(lista)
+numRep = (contRepeticiones(lista))
+
+while True:
+    print("Selecciona una opcion \n")
+    print("1-Contar numeros repetidos")
+    print("2-Eliminar repetidos")
+    print("3-Reemplazar repetidos con 0")
+    opcion = int(input())
+    
+    match opcion:
+        case 1:
+            k = 10
+            while k < 20:
+
+                l = 0
+                while l < len(lista):
+                    
+                    if lista[l] == k:
+                        print("El número", k, "aparece", numRep[l], "veces")
+                        break
+                    
+                    l += 1
+                
+                k += 1
+                
+        
+        case 2:
+            
+            lista2 = eliminarRepetidos(lista)
+            print("La nueva lista sin valores repetidos es:", lista2)
+            
+        case 3:
+            
+            print("La lista con valores repetidos a 0 es:", reemplazarACero(lista))
+
+
+
+
+
+
+
+
+
+
+# m = 1
+# while m < 10:
+
+#     n = 0
+#     while n < len(lista):
+        
+#         if numRep[n] > 1 and lista[n] == m:
+            
+#             del lista[n]
+#             print(lista)
+            
+#             n = 0
+        
+#         n += 1
+    
+#     m += 1
+    
