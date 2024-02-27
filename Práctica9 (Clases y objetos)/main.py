ee8d21a2b352fd89ca32045713d23e50f46d06c8
Div3r <udhgjkf@gmail.com>
2024-02-26 20:28:43 -0600

    Ejemplo de encapsulamiento

--- Práctica9 (Clases y objetos)/main.py
+++ Práctica9 (Clases y objetos)/main.py
@@ -1,25 +1,47 @@
 from Personaje import * 
 from Arma import *
 
-# Crear instancias del objeto personaje
+# Solicitar datos del Spartan
+nombreS = input("Escribe el nombre del Spartan: \n")
+especieS = input("\n Escribe la especie del Spartan: \n")
+alturaS = float(input("\n Escribe la altura del Spartan: \n"))
 
-spartan = Personaje()
+
+#Solicitar datos del Némesis
+nombreN = input("Escribe el nombre del Némesis: \n")
+especieN = input("\n Escribe la especie del Némesis: \n")
+alturaN = float(input("\n Escribe la altura del Némesis: \n"))
+
+
+# Crear instancias del objeto personaje
+spartan = Personaje(nombreS, especieS, alturaS)
+nemesis = Personaje(nombreN, especieN, alturaN)
 arma = Arma()   
 
 # Atributos del Spartan
-
+print("-- El Spartan contiene --")
 print(spartan.nombre)
 print(spartan.especie)
 print(spartan.altura)
 
+# Atributos del Némesis
+print("-- El Némesis contiene --")
+print(nemesis.nombre)
+print(nemesis.especie)
+print(nemesis.altura)
 
 # Métodos del Spartan
-
-spartan.correr(True)
+print("-- Métodos del Spartan --")
+spartan.correr(False)
 spartan.lanzarGranada()
+print("\n")
 
+# Métodos del Némesis
+print("-- Métodos del Némesis --")
+nemesis.correr(True)
+nemesis.lanzarGranada()
+print("\n")
 
 # Métodos del arma
-
 arma.seleccionarArma(spartan.nombre)
 arma.recargarArma(50)
--- Práctica9 (Clases y objetos)/personaje.py
+++ Práctica9 (Clases y objetos)/personaje.py
@@ -1,10 +1,15 @@
 class Personaje:
     # Atributo de personaje
-    especie = "Humano"
-    nombre = "John"
-    altura = 2.18
     
-    # Métodos del personaje
+    # Métodos del personaje --------
+    
+    # Crear método constructor
+    def __init__(self, nombre, especie, altura):
+        self.nombre = nombre
+        self.especie = especie
+        self.altura = altura
+    
+    
     def correr(self, estado):
         if(estado):
             print("El personaje",self.nombre, "está corriendo")
