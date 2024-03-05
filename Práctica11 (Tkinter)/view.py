b442e9af9e7e4758d73b7030650becc7f263ad09
Div3r <udhgjkf@gmail.com>
2024-03-04 20:13:11 -0600

    Práctica 11 terminada

--- Práctica11 (Tkinter)/view.py
+++ Práctica11 (Tkinter)/view.py
@@ -1,4 +1,25 @@
-from tkinter import Tk, Frame
+from tkinter import Tk, Frame, Button, messagebox
+
+
+# Método para mensaje
+def mostrarMensajes():
+    print(messagebox.showinfo('showinfo','Information'))
+    print(messagebox.showerror('showerror','Error'))
+    print(messagebox.showwarning('showwarning','Warning'))
+    print(messagebox.askokcancel(message="¿Desea Continuar?"),title="Soy un titulo")
+
+# Métodos 
+def addbton():
+    botonVerde.config(text="+")
+    botonRosa = Button(seccion3,text='rosa', fg="black", bg="pink")
+    botonRosa.configure(width='10',height='1')
+    botonRosa.pack()
+
+
+
+
+
+
 
 # 1- Crear la ventana 
 
@@ -19,6 +40,34 @@ seccion2.pack(expand=True,fill='both')
 
 seccion3 = Frame(ventana,bg="green")
 seccion3.pack(expand=True,fill='both')
+
+
+#3- Posicionar botones
+
+# Place
+botonAzul = Button(seccion1,text='azul', fg="darkblue")
+botonAzul.place(x=60,y=60, width='100',height='30')
+
+# Grid
+botonNegro = Button(seccion2,text='negro', fg="#FFFFFF", bg="#000000")
+botonNegro.configure(width='10',height='1')
+botonNegro.grid(row=0,column=1)
+
+botonAmarillo = Button(seccion2,text='amarillo', fg="black", bg="yellow", command=mostrarMensajes)
+botonAmarillo.configure(width='10',height='1')
+botonAmarillo.grid(row=2,column=2)
+
+# Pack
+
+botonVerde = Button(seccion3,text='verde', fg="black", bg="green",command=addbton)
+botonVerde.configure(width='10',height='1')
+botonVerde.pack()
+
+botonVerde2 = Button(seccion3,text='verde2', fg="black", bg="lightgreen")
+botonVerde2.configure(width='10',height='1')
+botonVerde2.pack()
+
+
 
 
 
