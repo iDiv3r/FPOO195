e548cf70d7f4de7c9431306ea513f6ba976e0d0f
Div3r <udhgjkf@gmail.com>
2024-03-09 18:28:41 -0600

    Práctica 13 terminada

--- Práctica13/GeneradorPass.py
+++ Práctica13/GeneradorPass.py
@@ -2,13 +2,14 @@ import random
 import string 
 
 class GeneradorPass:
-        
+    
+    letrasMinus = string.ascii_lowercase
+    numeros = string.digits
+    letrasMayus = string.ascii_uppercase
+    caracteresEspeciales = string.punctuation   
+
+
     def generarContraseña(self,especiales, mayusculas,longitud):
-        
-        letrasMinus = string.ascii_lowercase
-        numeros = string.digits
-        letrasMayus = string.ascii_uppercase
-        caracteresEspeciales = string.punctuation
         
         contraseñaNueva = ""
         
@@ -19,11 +20,11 @@ class GeneradorPass:
                 x =  random.randint(1,2)
                 
                 if x == 1:
-                    contraseñaNueva += random.choice(letrasMinus)
+                    contraseñaNueva += random.choice(self.letrasMinus)
                     i += 1
                     
                 else:
-                    contraseñaNueva += random.choice(numeros)
+                    contraseñaNueva += random.choice(self.numeros)
                     i += 1
             
             
@@ -34,15 +35,15 @@ class GeneradorPass:
                 x =  random.randint(1,3)
                 
                 if x == 1:
-                    contraseñaNueva += random.choice(letrasMinus)
+                    contraseñaNueva += random.choice(self.letrasMinus)
                     i += 1
                     
                 elif x == 2:
-                    contraseñaNueva += random.choice(numeros)
+                    contraseñaNueva += random.choice(self.numeros)
                     i += 1
                     
                 else:
-                    contraseñaNueva += random.choice(caracteresEspeciales)
+                    contraseñaNueva += random.choice(self.caracteresEspeciales)
                     i+= 1
             
             
@@ -53,15 +54,16 @@ class GeneradorPass:
                 x =  random.randint(1,3)
                 
                 if x == 1:
-                    contraseñaNueva += random.choice(letrasMinus)
+                    contraseñaNueva += random.choice(self.letrasMinus)
                     i += 1
                     
                 elif x == 2:
-                    contraseñaNueva += random.choice(numeros)
+                    contraseñaNueva += random.choice(self.numeros)
                     i += 1
                     
                 else:
-                    contraseñaNueva += random.choice(letrasMayus)
+                    contraseñaNueva += random.choice(self.letrasMayus)
+                    i += 1
             
             
         elif especiales == True and mayusculas == True:
@@ -71,18 +73,47 @@ class GeneradorPass:
                 x =  random.randint(1,4)
                 
                 if x == 1:
-                    contraseñaNueva += random.choice(letrasMinus)
+                    contraseñaNueva += random.choice(self.letrasMinus)
                     i += 1
                     
                 elif x == 2:
-                    contraseñaNueva += random.choice(numeros)
+                    contraseñaNueva += random.choice(self.numeros)
                     i += 1
                     
                 elif x == 3:
-                    contraseñaNueva += random.choice(letrasMayus)
+                    contraseñaNueva += random.choice(self.letrasMayus)
                     i += 1
+                    
                 else:
-                    contraseñaNueva += random.choice(caracteresEspeciales)
+                    contraseñaNueva += random.choice(self.caracteresEspeciales)
                     i += 1
         
         return contraseñaNueva
+    
+    def verificarContraseña(self,contraseña):
+        
+        numE = 0
+        numM = 0
+        
+        for caracter in contraseña:
+            if caracter in self.caracteresEspeciales:
+                numE += 1
+                
+            elif caracter in self.letrasMayus:
+                numM += 1
+        
+        
+        if len(contraseña) >= 8 and (numE <= (len(contraseña)/10) and numM <= (len(contraseña)/10)):
+            return 2
+        
+        elif len(contraseña) >= 8 and (numE >= (len(contraseña)/10) and numM <= (len(contraseña)/10)):
+            return 3
+        
+        elif len(contraseña) >= 8 and (numM >= (len(contraseña)/10) and numE <= (len(contraseña)/10)):
+            return 3
+        
+        elif len(contraseña) >= 8 and (numE >= (len(contraseña)/10) and numM >= (len(contraseña)/10)):
+            return 4
+        
+        else:
+            return 1
--- Práctica13/interfaz.py
+++ Práctica13/interfaz.py
@@ -1,76 +1,84 @@
-from tkinter import Tk, Frame, Button, messagebox, Label, Entry, ttk
+from tkinter import Tk, Frame, Button, messagebox, Label, Entry, ttk, Checkbutton, BooleanVar
 from GeneradorPass import *
 
 generador = GeneradorPass()
 
 def generarCont(especiales , mayusculas,longitud):
     
-    print (longitud)
-    
-    if especiales == "si":
-        op1 = True
-        
-    else:
-        op1 = False
-
-    if mayusculas == "si":
-        op2 = True
-        
-    else:
-        op2 = False
-        
-    
-    passNueva = generador.generarContraseña(op1,op2,longitud)
+    passNueva = generador.generarContraseña(especiales,mayusculas,longitud)
     
     print(messagebox.showinfo('Contraseña Nueva',("La nueva contraseña es:",passNueva)))
     
     txtNueva.delete(0,'end')
     txtNueva.insert(0,passNueva)
     
+
+def validarContraseña(contraseña):
+    validacion = generador.verificarContraseña(contraseña)
+    
+    match validacion:
+        case 1:
+            lblValidacion.config(text='La contraseña debil',bg='red')
+            
+        case 2:
+            lblValidacion.config(text='La contraseña es decente',bg='yellow')
+            
+        case 3:
+            lblValidacion.config(text='La contraseña es fuerte', bg='lightgreen')
+        
+        case 4:
+            lblValidacion.config(text='La contraseña es muy fuerte', bg='green')
     
 
 contraseña = Tk() 
-contraseña.title('Ejemplo con 3 frames')
-contraseña.geometry('600x600')
+contraseña.title('Generar Contraseñas')
+contraseña.geometry('250x400')
 
-fondo = Frame(contraseña,bg="gray")
+fondo = Frame(contraseña,bg="white")
 fondo.pack(expand=True,fill="both")
 
-lblLongitud = Label(fondo,text='Escribe la longitud')
-lblLongitud.place(x='250',y='50')
+#-- 
+
+lblLongitud = Label(fondo,text='Escribe la longitud',bg='white')
+lblLongitud.place(x='10',y='60')
 
 txtLongitud = Entry(fondo)
 txtLongitud.configure(width='30')
-txtLongitud.place(x='250',y='100')
+txtLongitud.place(x='10',y='100')
 
 #---
-
-lblEspeciales = Label(fondo,text='¿Deseas caracteres especiales?')
-lblEspeciales.place(x='250',y='150')
-
-txtEspeciales = Entry(fondo )
-txtEspeciales.configure(width='30')
-txtEspeciales.place(x='250',y='200')
+var1 = BooleanVar()
+chkEspeciales = Checkbutton(fondo,text="Caracteres especiales",bg='white',variable=var1)
+chkEspeciales.place(x='10',y='120')
 
 #----
 
-lblMayusculas = Label(fondo,text='¿Deseas mayusculas?')
-lblMayusculas.place(x='250',y='250')
-
-txtMayusculas = Entry(fondo)
-txtMayusculas.configure(width='30')
-txtMayusculas.place(x='250',y='300')
+var2 = BooleanVar()
+chkMayusculas = Checkbutton(fondo,text="Mayusculas",bg='white', variable=var2)
+chkMayusculas.place(x='10',y='150')
 
 #--
 
-btnIngresar = Button(fondo,text="Ingresar",bg='blue',command=lambda: generarCont(txtEspeciales.get(),txtMayusculas.get(), int(txtLongitud.get().strip() or 8) ))
-btnIngresar.configure(width='30',height='2')
-btnIngresar.place(x='250',y='350')
+btnGenerar = Button(fondo,text="Crear contraseña",bg='lightgray',command=lambda: generarCont(var1.get(),var2.get(), int(txtLongitud.get().strip() or 8) ))
+btnGenerar.configure(width='15',height='1')
+btnGenerar.place(x='10',y='200')
 
 #---
 
 txtNueva = Entry(fondo)
 txtNueva.configure(width='30')
-txtNueva.place(x='250',y='450')
+txtNueva.place(x='10',y='240')
+
+#---
+
+btnValidar = Button(fondo,text="Validar contraseña",bg='lightgray',command=lambda: validarContraseña(txtNueva.get()))
+btnValidar.configure(width='15',height='1')
+btnValidar.place(x='10',y='270')
+
+#--
+
+lblValidacion = Label(fondo,bg='white')
+lblValidacion.place(x='10',y='300')
+
 
 contraseña.mainloop()
